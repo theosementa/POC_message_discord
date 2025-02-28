14110d920dfbc0fe84932f68a820422397bde31f
Theo Sementa <theosementa@gmail.com>
2025-02-28 11:21:40 +0100

    fix: scrollable on Chrome

--- src/App.tsx
+++ src/App.tsx
@@ -1,36 +1,44 @@
-import { observer } from 'mobx-react'
-import { MessageStore } from './stores/message.store'
-import './styles/App.css'
+import { observer } from 'mobx-react';
+import { useEffect, useRef } from "react";
+import { MessageStore } from './stores/message.store';
+import './styles/App.css';
 
 const App = observer(() => {
-  const messageStore = MessageStore.shared
+  const messageStore = MessageStore.shared;
+  const messagesEndRef = useRef<HTMLDivElement | null>(null);
+
+  useEffect(() => {
+    messagesEndRef.current?.scrollIntoView({ behavior: "smooth" });
+  }, [messageStore.messages.length]);
 
   return (
-    <div className='flex flex-col h-screen w-screen justify-end p-4 gap-4'>
-      <div className='flex flex-col overflow-y-auto justify-end'>
-      {messageStore.messages.map(($0, index) => (
-      <p key={index}>{$0}</p>
-    ))}
+    <div className='flex flex-col h-screen w-screen p-4 gap-4'>
+      <div className='flex flex-col flex-grow h-64 overflow-y-auto'>
+        <div className='flex-grow'></div>
+        {
+          messageStore.messages.map((msg, index) => (
+            <p key={index}>{msg}</p>
+          ))
+        }
+        <div ref={messagesEndRef} />
       </div>
       <div className='flex flex-row justify-center gap-4 w-full'>
         <input
           className='bg-gray-900 px-4 rounded-lg w-full'
           type='text'
-          placeholder='Enter a message here ....'
+          placeholder='Entrez un message ici...'
           value={messageStore.currentMessage}
-          onChange={(e) => messageStore.currentMessage = e.target.value}
+          onChange={(e) => (messageStore.currentMessage = e.target.value)}
           onKeyDown={(e) => {
             if (e.key === "Enter") {
-              messageStore.sendMessage()
+              messageStore.sendMessage();
             }
-          }}  
-        ></input>
-        <button
-          onClick={() => messageStore.sendMessage()}
-        >Envoyer</button>
+          }}
+        />
+        <button onClick={() => messageStore.sendMessage()}>Envoyer</button>
       </div>
     </div>
-  )
-})
+  );
+});
 
-export default App
+export default App;
